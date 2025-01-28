8d6d5ce3ea7c9777ab6040aba9556f91dade0555
RomanPrikaztsikov <mikolkyle@gmail.com>
2025-01-28 15:30:35 +0200

    Update opilane.sql

--- opilane.sql
+++ opilane.sql
@@ -36,3 +36,40 @@ select * from opilane;
 --andmete uuendamine
 UPDATE opilane SET aadress='Tallinn'
 WHERE opilaneID=3
+
+CREATE TABLE Language
+(
+ID int NOT NULL PRIMARY KEY,
+Code char(3) NOT NULL,
+Language varchar(50) NOT NULL,
+IsOfficial bit,
+Percentage smallint
+); 
+
+Select * from Language
+
+INSERT INTO Language(ID, Code, Language)
+VALUES (1,'EST', 'eesti'), (3,'ENG', 'Inglise'),(4,'DE', 'saksa')
+
+Create table keelevalik(
+keelevalikID int primary key identity(1,1),
+valikuNimetus varchar(10), not null,
+opilaneID int,
+Foreign key(Language), references Language(ID)
+)
+select * from keelevalik;
+select * from Language;
+select * from opilane;
+
+INSERT INTO keelevalik(valikuNimetus, opilaneID, Language)
+Values ('valik B', 3, 4)
+
+SELECT opilane.eesnimi, Language.Language
+FROM opilane, Language keelevalik
+WHERE opilane.opilaneId=keelevalik.opilaneID
+AND Language.ID=keelevalik.Language
+
+SELECT * 
+FROM opilane, Language keelevalik
+WHERE opilane.opilaneId=keelevalik.opilaneID
+AND Language.ID=keelevalik.Language
