77801a374bd2007c704d6701d8fbc477ca59d5eb
RomanPrikaztsikov <mikolkyle@gmail.com>
2025-01-31 08:52:33 +0200

    Update opilane_localhost_XAMPP.sql

--- opilane_localhost_XAMPP.sql
+++ opilane_localhost_XAMPP.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE opilane(
+opilaneId int primary key AUTO_INCREMENT,
+eesimi varchar(25) not null,
+perenimi varchar(25) not null,
+synniaeg date,
+stip bit,
+aadress text,
+keskmine_hinne decimal(2,1)
+);
